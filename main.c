347196f505e932baa07bf1897e4db681dcd6a2be
Adam Raji <adraji@e1r11p5.1337.ma>
2025-11-29 16:15:57 +0100

    check bugs

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int	main() {
 
 	start = clock();
 	int fd = open("t2.txt", O_RDONLY);
-	char *line = "";// = get_next_line(fd);
+	char *line = "";
 	while (line)
 	{
 		line = get_next_line(10);
@@ -28,44 +28,3 @@ int	main() {
 
 	return 0;
 }
-
-/*int	main() {
-	clock_t start, end;
-	double cpu_time_used;
-
-	start = clock();
-	int fd1 = open("t1.txt", O_RDONLY);
-	int fd2 = open("t2.txt", O_RDONLY);
-	char *line1 = get_next_line(fd1);
-	char *line2 = get_next_line(fd2);
-	while (line1 && line2)
-	{
-		printf("%s", line1);
-		printf("%s", line2);
-		free(line1);
-		free(line2);
-		line1 = get_next_line(fd1);
-		line2 = get_next_line(99);
-		// if (i == 10)
-		// 	break;
-	}	
-	while (line1)
-	{
-		printf("%s", line1);
-		free(line1);
-		line1 = get_next_line(fd1);
-		// if (i == 10)
-		// 	break;
-	}
-	free(line1);
-	close(fd1);
-	close(fd2);
-
-	end = clock();
-
-	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-	printf("\nrun time: %fs for file \n", cpu_time_used);
-
-	return 0;
-}
-*/
